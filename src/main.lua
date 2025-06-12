11dcfd52a4be2e598020d53fa1cb71ab221515e9
uosq <159667110+uosq@users.noreply.github.com>
2025-06-12 17:57:36 -0300

    first commit

---
+++ src/main.lua
@@ -0,0 +1,12 @@
+--- made by navet
+--- started in 12/06/2025
+
+local tabs = {
+	aimbot = {},
+	trigger = {},
+	esp = {},
+	radar = {},
+	misc = {},
+	lobby = {}, --- not sure how we should make this
+	configs = {},
+}
